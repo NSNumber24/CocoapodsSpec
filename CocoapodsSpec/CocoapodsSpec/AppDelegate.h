75fdd00b05e4328c7733cc6ec3bb3817f1fb85ad
weirenxin <feixin510@qq.com>
2017-08-13 13:47:00 +0800

    初始化工程

---
+++ CocoapodsSpec/CocoapodsSpec/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoapodsSpec
+//
+//  Created by rx_wei on 2017/8/13.
+//  Copyright © 2017年 韦仁信. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
